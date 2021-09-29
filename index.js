ad97880154c20780af63e6b6d9fee34c0163b729
michael-lugashi <89575620+michael-lugashi@users.noreply.github.com>
2021-09-30 01:42:07 +0300

    Built The Basic Snake Grid
    I filled in the snakeGrid with elements. The game will be played on this grid.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 'use strict'
+
+buildSnakeGrid()
+
+function buildSnakeGrid() {
+    const snakeGrid = []
+    for (let i = 0; i < 15; i++) {
+        const gridRow = []
+        for (let j = 0; j < 15; j++) {
+            const backGroundColor= i%2 ? ['Grey', 'Black'][j%2]: ['Black', 'Grey'][j%2]
+            const gridSpace = createElem('div', `gridSpace${backGroundColor}`)
+            gameContainer.append(gridSpace)
+            gridRow.push(gridSpace)
+        }
+        snakeGrid.push(gridRow)
+    }
+}
+
+function createElem(tagName, cls) {
+    const newElem = document.createElement(tagName)
+    newElem.classList.add(cls)
+    return newElem
+}
